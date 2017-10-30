3c51f2e09e07956c8844a333aadc10c372cde61e
WispX <1591788658@qq.com>
2017-10-30 17:29:58 +0800

    update

--- lsky.sql
+++ lsky.sql
@@ -33,7 +33,7 @@ CREATE TABLE `article` (
   `send_time` int(11) NOT NULL COMMENT '发布时间',
   `is_delete` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否删除',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,8 +42,34 @@ CREATE TABLE `article` (
 
 LOCK TABLES `article` WRITE;
 /*!40000 ALTER TABLE `article` DISABLE KEYS */;
-INSERT INTO `article` VALUES (1,'男演员',1,'1591788658','这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录','0.0.0.0',1,0,0),(2,'女演员',2,'1591788658','这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录','0.0.0.0',0,0,0),(3,'未知演员',0,'1591788658','这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录这是一条测试记录','0.0.0.0',0,0,0),(4,'WispX',1,'1591788658','WispX','127.0.0.1',0,1509028908,0),(5,'WispX',1,'1591788658','卧槽','127.0.0.1',0,1509029261,0),(6,'WispX',1,'1591788658','卧槽','127.0.0.1',0,1509029309,0),(7,'WispX',1,'1591788658','卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽卧槽','127.0.0.1',0,1509029424,0),(8,'WispX',2,'1591788658','测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试','127.0.0.1',0,1509029711,0);
+INSERT INTO `article` VALUES (4,'WispX',1,'1591788658','测试表情：{face:二哈}','127.0.0.1',0,1509170213,0);
 /*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `content` text COLLATE utf8_unicode_ci NOT NULL COMMENT '评论内容',
+  `os` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT '操作系统',
+  `ip` char(32) COLLATE utf8_unicode_ci NOT NULL COMMENT '评论IP',
+  `add_time` int(11) NOT NULL COMMENT '评论时间',
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='评论表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -55,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-26 22:56:06
+-- Dump completed on 2017-10-30 17:28:30
