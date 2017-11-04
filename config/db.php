e5954502c31015b7b31f24216ee0cbc17c3a3953
WispX <1591788658@qq.com>
2017-11-04 18:17:47 +0800

    update

--- config/db.php
+++ config/db.php
@@ -8,9 +8,15 @@
  * Link: http://gitee.com/wispx
  */
 
+// 数据库连接地址
 define('DB_HOST', 'localhost');
+// 数据库用户名
 define('DB_USER', 'root');
+// 数据库密码
 define('DB_PASS', 'root');
+// 数据库名
 define('DB_NAME', 'test');
+// 数据库连接端口
 define('DB_PORT', '3306');
+// 数据库编码
 define('DB_CHARSET', 'utf8');
